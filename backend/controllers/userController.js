0560d2209c90816def63d96ec1df39da4d74ce48
youngen1 <122107840+youngen1@users.noreply.github.com>
2025-04-02 18:19:58 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -641,8 +641,8 @@ exports.resetPassword = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "Invalid token or user not found" });
     }
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-    user.password = hashedPassword;
+    
+    user.password = newPassword;
 
     await user.save();
 
