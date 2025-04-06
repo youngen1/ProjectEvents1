9a24d232c614205e135a83daafb4e001e0add4ee
youngen1 <122107840+youngen1@users.noreply.github.com>
2025-04-06 12:10:40 +0200

    Update PlatformEarnings.jsx

--- frontend/src/pages/Admin/PlatformEarnings.jsx
+++ frontend/src/pages/Admin/PlatformEarnings.jsx
@@ -12,7 +12,7 @@ const PlatformEarnings = () => {
         const fetchEarnings = async () => {
             try {
                
-                const response = await axiosInstance.get('/events/admin/earnings');
+                const response = await axiosInstance.get('/platform-earnings');
                 if (response.data) {
                     setEarnings(response.data);
                 } else {
